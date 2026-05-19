8c773b4df2c8cfba1b8a1d96bbfba59244465e97
OFFMN-SHARP <198691094+OFFMN-SHARP@users.noreply.github.com>
2026-05-19 12:43:39 +0800

    Add files via upload

--- start.ps1
+++ start.ps1
@@ -1,1 +1,9 @@
-qemu-system-i386 -kernel kernel.bin
+#qemu-system-i386 -kernel kernel.bin
+# run.ps1
+$img = "disk.img"
+if (-not (Test-Path $img)) {
+    Write-Host "Creating disk image (128M)..."
+    qemu-img create -f raw $img 128M
+}
+Write-Host "Starting SiNetDOS..."
+qemu-system-i386 -kernel kernel.bin -hda $img
